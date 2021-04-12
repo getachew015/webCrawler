7137567dc55e904222213172544a6e0c541b00f9
Dagim Getachew <getac005@umn.edu>
2021-04-12 05:21:42 -0500

    Added dependencies and modified app configuration

--- src/main/java/com/dagim/webCrawler/WebCrawlerApplication.java
+++ src/main/java/com/dagim/webCrawler/WebCrawlerApplication.java
@@ -1,13 +1,36 @@
 package com.dagim.webCrawler;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableAsync
 public class WebCrawlerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebCrawlerApplication.class, args);
 	}
 
+	@Bean(name = "webCrawlerTaskExecutor")
+    public Executor taskExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("WebCrawlerThread");
+        executor.initialize();
+        
+        return executor;
+    }
+	
+	
 }
